0b750133e01fa1833c5fb88ebea73646918abd19
tayo <s1rpentwhat@gmail.com>
2023-10-25 16:46:22 -0300

    feat: add response for user interaction

--- cogs/tracker.py
+++ cogs/tracker.py
@@ -1,5 +1,6 @@
 import nextcord
 import requests
+from datetime import datetime
 
 from nextcord.ext import commands
 from nextcord import Interaction, SlashOption
@@ -27,10 +28,53 @@ class Tracker(commands.Cog):
       description='Enter user battletag to get user stats'
     )
   ):
-    request = request.get(f"https://ow-api.com/v1/stats/{platform}/us/{battletag}/profile") 
+    """ Creating user request for player stats
+        Validation status code response from API
+    """
+    response = requests.get(f"https://ow-api.com/v1/stats/{platform}/us/{battletag.replace('#', '-')}/profile")
+    data = response.json()
 
-    # GET https://ow-api.com/v1/stats/:platform/:region/:battletag/profile
-    await interaction.response.send_message('')
+    author = interaction.user.mention
+    if response.status_code == 404:
+      await interaction.response.send_message(f"{author} - User {battletag} not found, please try again!")
+      return
+  
+    competitive_stats = data["competitiveStats"]
+    matchs = competitive_stats["games"]
+    played = matchs["played"]
+    won = matchs["won"]
+    ratings = data["ratings"]
+    thumbnail = data['icon']
+
+    description = []
+    for i in ratings:
+      elo = i["group"]
+      role = i["role"]
+      tier = i["tier"]
+
+      message = f"{role}: {elo} {tier}"
+      description.append(message)
+    d_message = '\n'.join(description)
+
+    embed = nextcord.Embed(
+      title=battletag,
+      description=d_message,
+      timestamp=datetime.utcnow()
+    )
+
+    embed.set_footer(
+      text="https://playscrim.com"
+    )
+
+    embed.add_field(
+      name="Matchs:",
+      value=f"Played: {played}\nWon: {won}"
+    )
+
+    embed.set_thumbnail(
+      url=thumbnail
+    )
+    await interaction.response.send_message(embeds=[embed])
 
 def setup(client):
   """Setup function to add cog to client
